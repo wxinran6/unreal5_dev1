6c1aedc3d035dab3b95867d17ce94ca75d436e1f
xinran <wxinran6@vt.edu>
2023-01-21 11:15:39 +0000

    exclude_starter_content

---
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	int32 MyInt = 99;
+
+	UPROPERTY(EditAnywhere)
+	float MyFloat = 6.66;
+
+	UPROPERTY(EditAnywhere)
+	bool MyBool = true;
+	
+	UPROPERTY(EditAnywhere)
+	FVector MyVector = FVector(1,2,3);
+};
